80c3af3cf63d19172ff8114bff66027277090305
Hadley Slucher <hadleyslucher@gmail.com>
2020-10-26 21:30:59 -0400

    Added contact page, Working on instagram verification

---
+++ javascript/script.js
@@ -0,0 +1,5 @@
+https://api.instagram.com/oauth/authorize
+  ?client_id=351734589220230
+  &redirect_uri=https://hadleyjpg.github.io/alicephoto/
+  &scope=user_profile,user_media
+  &response_type=code
