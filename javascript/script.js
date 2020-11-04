95a28811aed510bfe2813ac8003c18d6e277d06c
Hadley Slucher <hadleyslucher@gmail.com>
2020-11-04 01:51:52 -0500

    Creating portfolio gallery

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,96 @@
-https://api.instagram.com/oauth/authorize
-  ?client_id=351734589220230
-  &redirect_uri=https://hadleyjpg.github.io/alicephoto/
-  &scope=user_profile,user_media
-  &response_type=code
+const nameInput = document.getElementById("name");
+const phoneInput = document.getElementById("phone");
+const emailInput = document.getElementById("mail");
+
+/**
+ * 
+ * VALIDATORS
+ *  
+ */
+
+// The telephone number must be in the format of (555) 555-5555
+function isValidTelephone(phone) {
+    return /^\D*\d{3}\D*\d{3}\D*\d{4}\D*$/.test(phone);
+}
+
+// Must be a valid email address
+function isValidEmail(mail) {
+return /^[^@]+@[^@.]+\.[a-z]+$/i.test(mail);
+}
+
+/**
+ * 
+ * FORMATTING FUNCTIONS
+ * 
+ */
+
+function formatTelephone(text) {
+    const expression = /^\D*(\d{3})\D*(\d{3})\D*(\d{4})\D*$/;
+    return text.replace(expression, "($1) $2-$3");
+  }
+
+function createListener(validator) {
+return e => {
+    const text = e.target.value;
+    const valid = validator(text);
+    const showTip = text !== "" && !valid;
+    const tooltip = e.target.nextElementSibling;
+    showOrHideTip(showTip, tooltip);
+};
+}
+
+phoneInput.addEventListener("input", createListener(isValidTelephone));
+
+phoneInput.addEventListener("blur", e => {
+  e.target.value = formatTelephone(e.target.value);
+});
+
+emailInput.addEventListener("input", createListener(isValidEmail));
 
 
-  var = 2;
+/**
+ * 
+ * Lightbox Gallery
+ *  
+ */
+
+// Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
